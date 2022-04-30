2e9855bd8a42c35228a60a18710aa9e60f7fe951
akshay moradiya <akshay.trinityinfosystem@gmail.com>
2022-04-30 11:04:07 +0530

    not found route added && rollback to page routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.scss';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AppRoutes } from './Routes';
+import MainLayout from './Layout/Main/MainLayout';
+import Home from './pages/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <Router>
-      <AppRoutes />
+      <Routes>
+        <Route path='/' element={<MainLayout />} > 
+          <Route index element={<Home />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
+      {/* <AppRoutes /> */}
     </Router>
   );
 }
